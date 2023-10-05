5e9a2ebea53f34a591f7f5ea799719246e9d1b3d
Alejandro Casiva <alec667@gmail.com>
2023-10-05 20:34:00 -0300

    Test class added

--- src/test/java/com/practica/demojenkins/controller/MyControllerTest.java
+++ src/test/java/com/practica/demojenkins/controller/MyControllerTest.java
@@ -1,11 +1,20 @@
 package com.practica.demojenkins.controller;
 
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 public class MyControllerTest {
@@ -29,5 +38,16 @@ public class MyControllerTest {
         autoCloseable.close();
     }
 
-    
+    @Test
+    void helloTest() throws Exception {
+        ResponseEntity<String> response = new ResponseEntity<String>("Hello :)", HttpStatus.OK);
+        when(myController.hello()).thenReturn(response);
+
+        MockHttpServletRequestBuilder mockResponse = MockMvcRequestBuilders
+                .get("/rest/hello")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(mockResponse)
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
 }
