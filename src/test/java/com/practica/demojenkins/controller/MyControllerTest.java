f201aeece4286dc4e544e6744c11c0efb4820816
Alejandro Casiva <alec667@gmail.com>
2023-10-05 20:14:04 -0300

    more test setup

--- src/test/java/com/practica/demojenkins/controller/MyControllerTest.java
+++ src/test/java/com/practica/demojenkins/controller/MyControllerTest.java
@@ -1,10 +1,15 @@
 package com.practica.demojenkins.controller;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 public class MyControllerTest {
+    
     @Autowired
     private MockMvc mockMvc;
 
@@ -13,6 +18,16 @@ public class MyControllerTest {
 
     private AutoCloseable autoCloseable;
 
+    @BeforeEach
+    void setUP() {
+        autoCloseable = MockitoAnnotations.openMocks(this);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(myController).build();
 
-    
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        autoCloseable.close();
+    }
+
 }
